ae97d3a9d8959b2d0c0608ddfcba536aaa9a5f30
Tim Claes <tim.claes@me.com>
2018-07-22 12:32:31 +0200

    installing heroku dependancies

--- healthhouse/settings/production.py
+++ healthhouse/settings/production.py
@@ -6,3 +6,20 @@ try:
     from .local import *
 except ImportError:
     pass
+
+#
+# DATABASE SETUP
+#
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+#
+# STATIC FILES
+#
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+#
+# MEDIA FILES
+#
