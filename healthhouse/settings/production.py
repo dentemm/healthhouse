dee932b7cf0e3ecd23b81c3bc774a5e1d7d72802
Tim Claes <tim.claes@me.com>
2018-07-31 20:27:38 +0200

    setup aws S3 storage

--- healthhouse/settings/production.py
+++ healthhouse/settings/production.py
@@ -15,6 +15,10 @@ WSGI_APPLICATION = 'healthhouse.wsgi.application'
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
+#
+# DATABASE
+#
+
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
@@ -27,3 +31,15 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 #
 # MEDIA FILES
 #
+
+AWS_STORAGE_BUCKET_NAME = 'hhwebsite'
+# AWS_REGION = 'eu-central-1'
+# AWS_S3_HOST = 's3.eu-central-1.amazonaws.com'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.eu-central-1.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'home.custom_storages.MediaStorage'
