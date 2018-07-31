98734b92dd3666de26057c321897c452e8f7cc02
Tim Claes <tim.claes@me.com>
2018-07-31 20:46:34 +0200

    updated production settings

--- healthhouse/settings/production.py
+++ healthhouse/settings/production.py
@@ -15,6 +15,8 @@ WSGI_APPLICATION = 'healthhouse.wsgi.application'
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.health-house.be', '.aws.amazon.com', '.herokuapp.com']
+
 #
 # MIDDLEWARE
 #
