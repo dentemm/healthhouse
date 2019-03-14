d9a23b2a2b578a164c95b606cf4fab4527e596b6
Tim Claes <tim.claes@me.com>
2019-03-14 20:42:57 +0100

    working on calendar functionality

---
+++ calendar/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = () => {
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    }
+  }
+};
