134c6cb0ceea58a49bba1a02e64c153cd8e65f4b
Tim Claes <tim.claes@me.com>
2019-04-22 21:21:11 +0200

    small fix

--- home/models/__init__.py
+++ home/models/__init__.py
@@ -830,7 +830,7 @@ class EventListPage(Page):
     template = 'home/agenda_page.html'
 
     def events(self):
-        return Event.objects.all().filter(date_gte=date.today()).order_by('date')
+        return Event.objects.all().filter(date__gte=date.today()).order_by('date')
         # return Event.objects.all().filter(date__gte=date.today()).order_by('date')[0:4]
 
 EventListPage.content_panels = [
