ac271b7659e20918cb7acd86ec20bb0aecbda3a4
Tim Claes <tim.claes@me.com>
2019-12-04 15:21:56 +0100

    uploading new build to check speed

--- healthhouse/settings/base.py
+++ healthhouse/settings/base.py
@@ -182,6 +182,6 @@ WAGTAIL_SITE_NAME = "healthhouse"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://example.com'
+BASE_URL = 'http://health-house.be'
 
 PASSWORD_REQUIRED_TEMPLATE = 'home/events/login.html'
