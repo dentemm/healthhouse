6c0e04aa18ac6ad4fc4597a690357fed6d5c9965
Tim Claes <tim.claes@me.com>
2020-11-04 20:01:51 +0100

    added script

--- home/management/commands/clean_renditions.py
+++ home/management/commands/clean_renditions.py
@@ -9,21 +9,20 @@ class Command(BaseCommand):
     renditions = Rendition.objects.all()
     self.stdout.write(str(renditions.count())) 
 
-
-		# if renditions:
-		# 	images_root = os.path.join(settings.MEDIA_ROOT, 'images')
-		# 	message = ['\n']
-		# 	message.append('This will REMOVE ALL IMAGE RENDITION FILES in {}!\n'.format(images_root))
-		# 	message.append(
-		# 		'Are you sure you want to do this?\n\n'
-		# 		"Type 'yes' to continue, or 'no' to cancel: "
-		# 	)
-		# 	if input(''.join(message)) != 'yes':
-		# 		raise CommandError("Remove renditions cancelled.")
-		# 	else:
-		# 		# doe het
-		# 		renditions.delete()
-		# 		shutil.rmtree(images_root)
-		# 		self.stdout.write('Done')	
-		# else:
-		# 	self.stdout.write('There are no image renditions')
+    if renditions:
+      images_root = os.path.join(settings.MEDIA_ROOT, 'images')
+      message = ['\n']
+      message.append('This will REMOVE ALL IMAGE RENDITION FILES in {}!\n'.format(images_root))
+      message.append(
+        'Are you sure you want to do this?\n\n'
+        "Type 'yes' to continue, or 'no' to cancel: "
+      )
+      if input(''.join(message)) != 'yes':
+        raise CommandError("Remove renditions cancelled.")
+      else:
+				# doe het
+        renditions.delete()
+        shutil.rmtree(images_root)
+        self.stdout.write('Done')	
+    else:
+      self.stdout.write('There are no image renditions')
