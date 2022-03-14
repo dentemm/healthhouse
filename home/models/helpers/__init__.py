671b28343fddccc3d4cc1c0b8b9dd71918456618
Tim Claes <tim.claes@me.com>
2022-03-14 21:39:34 +0100

    chore: removing expo page

--- home/models/helpers/__init__.py
+++ home/models/helpers/__init__.py
@@ -5,45 +5,45 @@ from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, FieldRowPan
 from django_countries.fields import CountryField
 
 class TimeStampMixin(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        abstract = True
+	class Meta:
+		abstract = True
 
 class Address(models.Model):
 
-    city = models.CharField(max_length=40)
-    postal_code = models.CharField(max_length=8)
-    street = models.CharField(max_length=40)
-    number = models.CharField(max_length=8)
-    country = CountryField(null=True, default='BE')
+	city = models.CharField(max_length=40)
+	postal_code = models.CharField(max_length=8)
+	street = models.CharField(max_length=40)
+	number = models.CharField(max_length=8)
+	country = CountryField(null=True, default='BE')
 
-    class Meta:
-        verbose_name = 'address'
-        verbose_name_plural = 'addresses'
-        ordering = ['city', ]
-        
-    def __str__(self):
-        return '%s - %s' % (self.city, self.street)
+	class Meta:
+		verbose_name = 'address'
+		verbose_name_plural = 'addresses'
+		ordering = ['city', ]
+			
+	def __str__(self):
+		return '%s - %s' % (self.city, self.street)
 
 Address.panels = [
-		MultiFieldPanel([
-				FieldRowPanel([
-						FieldPanel('street', classname='col8'),
-						FieldPanel('number', classname='col4')
-				]
-		),
+	MultiFieldPanel([
 		FieldRowPanel([
-				FieldPanel('city', classname='col8'),
-				FieldPanel('postal_code', classname='col4')
+			FieldPanel('street', classname='col8'),
+			FieldPanel('number', classname='col4')
+		]
+	),
+	FieldRowPanel([
+		FieldPanel('city', classname='col8'),
+		FieldPanel('postal_code', classname='col4')
+	]),
+	FieldRowPanel([
+		FieldPanel('country', classname='col6'),
 		]),
-		FieldRowPanel([
-				FieldPanel('country', classname='col6'),
-				]),
-		],
-		heading='Address details'
-		)
+	],
+	heading='Address details'
+	)
 ]
 
 class GeneralBullet(models.Model):
